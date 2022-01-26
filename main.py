9aaa38ed7c65cef38df3f05081a0c742fa02e695
David <dchincharashvili@archive.gov.ge>
2022-01-26 17:35:42 +0400

    added io tests

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import subprocess
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 from configs import *
 import logging
 
@@ -93,26 +94,180 @@ def edit_type(tablename='testtable'):
 
 def delete_type():
     go_uri("/admin/types")
-    submit_btn = driver.find_element_by_css_selector("button.btn.btn-danger")
+    elem = driver.find_elements_by_css_selector("button.btn.btn-danger")[1]
+    elem.click()
+    alert = driver.switch_to.alert
+    print(alert.text)
+    alert.accept()
+
+
+def helper_add_io(prefix="pre", identifier='154', suffix="suf", fond="Fond name"):
+    pref = driver.find_element_by_css_selector("#prefix")
+    ident = driver.find_element_by_css_selector("#identifier")
+    suf = driver.find_element_by_css_selector("#suffix")
+    type = driver.find_element_by_css_selector("#type")
+
+    pref.clear()
+    pref.send_keys(prefix, Keys.TAB)
+
+    ident.clear()
+    ident.send_keys(identifier, Keys.TAB)
+
+    suf.clear()
+    suf.send_keys(suffix, Keys.TAB)
+
+    select_type = Select(type)
+    select_type.select_by_value("fonds")  # select fond
+    time.sleep(wait)
+
+    fond_name = driver.find_element_by_css_selector("#name")
+    fond_name.send_keys(fond)
+
+
+
+def add_io():
+    go_uri("/admin/io")
+    elem = driver.find_element_by_css_selector(".add-button span")
+    elem.click()
+    time.sleep(wait)
+
+    helper_add_io("1", 1, '1', 'fond1')
+
+    # save changes
+    submit_btn = driver.find_element_by_css_selector("button.btn.btn-success")
     submit_btn.click()
+
+
+def add_io_child():
+    go_uri("/admin/io/show/1")
+    elem = driver.find_element_by_css_selector(".add-button span")
+    elem.click()
+    time.sleep(wait)
+
+    helper_add_io("2", 2, '2', 'fond2')
+
+    # save changes
+    submit_btn = driver.find_element_by_css_selector("button.btn.btn-success")
+    submit_btn.click()
+
+def edit_io(prefix="p", identifier=1, suffix="s"):
+    go_uri("/admin/io")
+    elem = driver.find_elements_by_css_selector(".btn.btn-info")[0]
+    elem.click()
+
+    pref = driver.find_element_by_css_selector("#prefix")
+    ident = driver.find_element_by_css_selector("#identifier")
+    suf = driver.find_element_by_css_selector("#suffix")
+
+    pref.clear()
+    pref.send_keys(prefix, Keys.TAB)
+
+    ident.clear()
+    ident.send_keys(identifier, Keys.TAB)
+
+    suf.clear()
+    suf.send_keys(suffix, Keys.TAB)
+
+    # save changes
+    submit_btn = driver.find_element_by_css_selector("button.btn.btn-success")
+    submit_btn.click()
+
+def edit_io_child(prefix="c", identifier=1, suffix="c"):
+    go_uri("/admin/io")
+    elem = driver.find_elements_by_css_selector(".btn.btn-info")[1]
+    elem.click()
+
+    pref = driver.find_element_by_css_selector("#prefix")
+    ident = driver.find_element_by_css_selector("#identifier")
+    suf = driver.find_element_by_css_selector("#suffix")
+
+    pref.clear()
+    pref.send_keys(prefix, Keys.TAB)
+
+    ident.clear()
+    ident.send_keys(identifier, Keys.TAB)
+
+    suf.clear()
+    suf.send_keys(suffix, Keys.TAB)
+
+    # save changes
+    submit_btn = driver.find_element_by_css_selector("button.btn.btn-success")
+    submit_btn.click()
+
+
+def delete_io_child():
+    go_uri("/admin/io")
+    elem = driver.find_elements_by_css_selector(".btn.btn-danger")[1]
+    elem.click()
+
+    alert = driver.switch_to.alert
+    print(alert.text)
+    time.sleep(1)
+    alert.accept()
+
+
+def delete_io():
+    go_uri("/admin/io")
+    elem = driver.find_elements_by_css_selector(".btn.btn-danger")[0]
+    elem.click()
+
+    alert = driver.switch_to.alert
+    print(alert.text)
+    time.sleep(1)
+    alert.accept()
+
+################
+## types test ##
+################
 
 
 login(user, password)
 time.sleep(wait)
 
-add_type()
-time.sleep(wait)
 
-edit_type()
-time.sleep(wait)
+def types_test():
+    add_type()
+    time.sleep(wait)
 
-try:
+    edit_type()
+    time.sleep(wait)
+
     delete_type()
     time.sleep(wait)
+
+
+###################
+
+def io_test():
+    add_io()
+    time.sleep(wait)
+
+    add_io_child()
+    time.sleep(wait)
+
+    edit_io()
+    time.sleep(wait)
+
+    edit_io_child()
+    time.sleep(wait)
+
+    delete_io_child()
+    time.sleep(wait)
+
+    delete_io()
+    time.sleep(wait)
+
+
+types_test()
+io_test()
+
+
+try:
+    print("done")
 except:
     print("you have some problem")
 
 driver.close()
 
-if migration_end == "yes":
+if migration_end == "no":
     migrate_and_seed()
